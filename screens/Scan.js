574280d17449a48b9ec7eff3df0112b94ba993d7
pasinee phommuangdee <dofahzzz@gmail.com>
2024-02-05 15:43:21 +0700

    scan page

--- screens/Scan.js
+++ screens/Scan.js
@@ -1,11 +1,33 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
 
 const Scan = () => {
+  const [data, Setdata] = useState('scan something')
   return (
-    <View>
-      <Text>Scan</Text>
-    </View>
+    <QRCodeScanner
+      onRead={({ data }) => Setdata(data)}
+      reactivate={true}
+      reactivateTimeout={500}
+      showMarker={true}
+      topContent={
+        <View>
+          <Text style={{
+            color: 'black',
+            padding: 20,
+            fontSize: 20,
+            backgroundColor: 'pink',
+            margin: 10,
+          }}>{data}</Text>
+        </View>
+      }
+      bottomContent={
+        <View>
+          <Text>Barcode scanner</Text>
+        </View>
+      }
+    />
   )
 }
 
